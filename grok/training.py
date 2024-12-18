19ed880499406ed5672193269d8d34faf9e28cc7
Di Yue <di_yue@stu.pku.edu.cn>
2024-12-18 23:42:51 +0800

    new logs

--- grok/training.py
+++ grok/training.py
@@ -107,7 +107,8 @@ def train(hparams: Namespace) -> None:
     }
     
     if torch.cuda.is_available() and hparams.gpu >= 0:
-        trainer_args["gpus"] = [hparams.gpu]
+        trainer_args["devices"] = [hparams.gpu]  # List of GPU indices (e.g., [0])
+        trainer_args["accelerator"] = "gpu"     # Use GPU as accelerator
 
     trainer = Trainer(**trainer_args)
 
@@ -254,7 +255,7 @@ def add_args(parser=None) -> Namespace:
     parser.add_argument("--random_seed", type=int, default=-1)
     parser.add_argument("--gpu", type=int, default=0)
     parser.add_argument("--max_epochs", type=int, default=None)
-    parser.add_argument("--max_steps", type=int, default=100000)
+    parser.add_argument("--max_steps", type=int, default=500000)
     # parser.add_argument("--load_ckpt", dest="load_ckpt", action="store_true")
     # parser.set_defaults(load_ckpt=False)
     parser.add_argument("--ckpt_epoch", type=str, default="0")
